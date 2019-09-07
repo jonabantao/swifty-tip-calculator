5e374926c1ef86e80159481dd0bc853ace9e3f47
Jonathan Abantao <jonabantao@gmail.com>
2019-09-07 10:29:19 -0700

    Initial Commit

---
+++ SwiftyTipCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftyTipCalculator
+//
+//  Created by Jonathan Abantao on 9/7/19.
+//  Copyright © 2019 Jonathan Abantao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
